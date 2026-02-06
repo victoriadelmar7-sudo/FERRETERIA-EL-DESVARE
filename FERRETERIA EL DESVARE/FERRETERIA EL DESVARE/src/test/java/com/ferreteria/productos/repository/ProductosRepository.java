29fca4ddafcc84e67766aca668da50af70269a7c
victoriadelmar7-sudo <victoriadelmar7@gmail.com>
2026-02-05 19:43:29 -0500

    Delete FERRETERIA EL DESVARE/FERRETERIA EL DESVARE directory

--- FERRETERIA EL DESVARE/FERRETERIA EL DESVARE/src/test/java/com/ferreteria/productos/repository/ProductosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ferreteria.productos.repository
-;
-import com.ferreteria.productos.model.Productos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-public interface ProductosRepository extends JpaRepository<Productos, Long> {
-
-}
