e8f0b51d479783c4e7421a72c4885d6b28a96d6f
victoriadelmar7-sudo <victoriadelmar7@gmail.com>
2026-02-05 19:43:15 -0500

    sena

---
+++ Ferreteria el Desvare/Ferreteria El Desvare/src/test/java/com/ferreteria/eldesvare/FerreteriaElDesvareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferreteria.eldesvare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerreteriaElDesvareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
